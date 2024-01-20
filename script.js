53cd148d765106b8406c9f382cef1c322c86db84
Muhammadcodes112 <funuyallen@gmail.com>
2024-01-19 23:22:03 -0800

    button Hover Effects

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 // code for loader
-// document.addEventListener("DOMContentLoaded", function () {
-//     var loader = document.getElementById("loader");
-//     var content = document.getElementById("content");
-
-//     setTimeout(function () {
-//         loader.style.display = "none";
-//         content.style.display = "block"; 
-//     }, 2000);
-// });
-
-// code for loader
 document.addEventListener("DOMContentLoaded", function () {
     var loader = document.getElementById("loader");
     var content = document.getElementById("content");
@@ -50,31 +39,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-
-const sr = ScrollReveal ({
-    distance: '650px',
-    duration: '2600',
-    delay: 450,
-    reset: true
-})
-
-//sr.reveal('.header', {delay: 200, origin:'bottom'})
-sr.reveal('.home', {delay: 20, origin:'bottom'})
-sr.reveal('.courses', {delay: 20, origin:'bottom'})
-sr.reveal('.box', {delay: 20, origin:'bottom'})
-sr.reveal('.subjects', {delay: 20, origin:'bottom'})
-sr.reveal('.learners', {delay: 20, origin:'bottom'})
-sr.reveal('.facilities', {delay: 20, origin:'bottom'})
-sr.reveal('.facilities-col', {delay: 20, origin:'bottom'})
-sr.reveal('.testimonials', {delay: 20, origin:'bottom'})
-sr.reveal('.testimonials-col', {delay: 20, origin:'bottom'})
-sr.reveal('.cta', {delay: 20, origin:'bottom'})
-sr.reveal('.p-btn', {delay: 20, origin:'left'})
-sr.reveal('.footer', {delay: 20, origin:'bottom'})
-sr.reveal('.logo', {delay: 20, origin:'top'})
-sr.reveal('.container', {delay: 20, origin:'bottom'})
-sr.reveal('.blog-left', {delay: 20, origin:'bottom'})
-sr.reveal('.blog-right', {delay: 20, origin:'bottom'})
-sr.reveal('.comment-box', {delay: 20, origin:'bottom'})
 
 
